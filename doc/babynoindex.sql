2554a25fc5619a3382f818604c924a9c28bcaf9c
myhelloworid666 <2210602496@qq.com>
2018-09-27 15:13:35 +0800

    所有表单验证已完成，编辑宝贝表单存在小问题，默认显示的父母总是用户列表第一个

--- doc/babynoindex.sql
+++ doc/babynoindex.sql
@@ -51,4 +51,4 @@ CREATE TABLE bb_blog
 
 
 -- 添加一个管理员 username:admin password:admin
-INSERT INTO bb_user (name, password, gm, amilymembers) VALUES ("admin","21232f297a57a5a743894a0e4a801fc3",5,"baba");
+INSERT INTO bb_user (name, password, gm, amilymembers) VALUES ("admin","21232f297a57a5a743894a0e4a801fc3",5,"爸爸");
